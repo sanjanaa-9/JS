4dbe2b208e9e5f99b27576da6b6de219096e145a
unknown <sanjnameena74@gmail.com>
2025-02-05 16:18:19 -0500

    solve 10 js QUETIONS

--- script.js
+++ script.js
@@ -1,4 +1,133 @@
 // 100  Questions 
-//  Question-1
-//
+// 1- BASIC CONSOLE USAGE [10 QUETIONS]
+
+// Q-1 Log . your  name and favriot hobby to the console.
+// solve
+ 
+console.log("sanjana","reading books");
+
+// Q-2 Perform and log the result of 45*2 -10.
+//solve
+console.log(45*2-10);
+
+// Q-3 use console.log() to display the current year
+ const year = new Date();
+ console.log(year.getFullYear());
+
+// Q-4 create two variables for firts and last name concatenated and log them.
+// solve
+  var first = "Sanjana"
+  var last = "Harsh"
+  console.log(first + " " + last);
+
+  // one another way to solve this -
+
+  console.log(`${first} ${last}`);
+
+// Q-5 track the value of a variable by logging it before and after updating.
+// solve
+var a = 2025;
+console.log(a);
+var b = 2026;
+console.log(b);
+// Q-6 use console.error() to simulate an error message.
+//solve 
+console.error("Something went wrong");
+// Q-7 Log the squre of the number 12 to the console.
+console.log(12*12);
+
+// Q-8 print the  type of a variable holding the value true .
+var a = true;
+console.log(typeof a);
+
+// Q-9 create a variable holding your age and log whether it's greater than 18. 
+// solve
+var age = 20;
+if (age > 18) console.log(true);
+else console.log(false);
+// Q-10 Log a result of 100/0 and observe the output 
+console.log(100/0);
+
+// 2- Variables and Data Types [10 QUESTIONS]
+
+// Q-11 Decalre a variable using let and log its value.
+// Q-12 create a constant to store the value of PI and log it.
+// Q-13 Reassign a value to a varible declared with let and log its result.
+// Q-14 check the type of null and log it 
+// Q-15 create a variable with a number as a string (e.g. "25") and log its types.
+// Q-16 use typeof to check the type of a buolean variable
+// Q-17 create three variables of types string, number, and boolean and log their values
+// Q-18 declare a variable without assigning a value log its type.
+// Q-19 create a variable with undefined and log its type.
+// Q-20 use const to create an array try reassigning the array and observe the error .
+
+
+// 3- Loops [10 QUESTIONS]
+
+// Q-21 write a for loop to print numbers from 1 to 50 .
+// Q-22 use a while loop to sum the number from 1 to 10
+// Q-23 create a for....of loop to log each character of the string "javascript"
+// Q-24 write a for loop that skips even number between 1 to 20.
+// Q-25 use a do...while loop to log number from 5 to 1 
+// Q-26 create a for loop that calculates the factorial of 5 
+// Q-27 write a nested loop to print a 3x3 grid of numbers 
+// Q-28 use a for loop to reverse an array [1,2,3 ,4].
+// Q-29 write a while loop that logs numbers from 1 to 100 divisible by 5.
+// Q-30 use A for ...in loop to iterate  over an object and log its keys. 
+
+
+// Arrays [10 QUESTIONS]
+
+// Q-31 
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+// Q-11
+
+
 
